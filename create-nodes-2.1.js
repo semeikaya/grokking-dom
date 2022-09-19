2cd066c42ded63814ec65c9fd8b0266d26cf5cc9
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-19 13:57:14 +0300

    update

--- create-nodes-2.1.js
+++ create-nodes-2.1.js
@@ -1,9 +1,10 @@
 const arr = ['html', 'css', 'js']
 const ul = document.createElement('ul')
+
 for (const ar of arr) {
     const li = document.createElement('li')
     li.textContent = ar
     ul.append(li)
 }
-const body = document.createElement('body')
-body.append(ul)
+
+document.body.append(ul)
