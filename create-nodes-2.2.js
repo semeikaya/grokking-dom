cc63caec202e14730a32afca4d56eca6d6f8b2f7
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-19 14:02:56 +0300

    update

--- create-nodes-2.2.js
+++ create-nodes-2.2.js
@@ -1,30 +1,13 @@
-// Вариант 1
 function render(arr) {
-    const body = document.createElement("body");
     const ul = document.createElement("ul");
+    
     for (const ar of arr) {
         const li = document.createElement("li");
         li.textContent = ar;
         ul.append(li);
     }
-    body.append(ul);
-    return body;
-}
-
-
-// Вариант 2
-function render1(arr) {
-    const body = document.createElement("body");
-    for (const ar of arr) {
-        const ul = document.createElement("ul");
-        for (const ar of arr) {
-            const li = document.createElement("li");
-            li.textContent = ar;
-            ul.append(li);
-        }
-        body.append(ul);
-    }
-    return body;
+    document.body.append(ul);
+    return document.body;
 }
 
 console.log(render(["html", "css", "js"]));
