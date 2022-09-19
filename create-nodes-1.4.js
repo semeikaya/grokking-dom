4d60a69c7a7e9a97458ddcc2e44462e6ba772e16
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-19 12:47:14 +0300

    update

--- create-nodes-1.4.js
+++ create-nodes-1.4.js
@@ -1,7 +1,9 @@
 const ul = document.createElement('ul')
-ul.classList.add('list')
 const li = document.createElement('li')
+
+ul.className = 'list'
 li.textContent = 'привет мир'
 ul.append(li)
+
 console.log(ul);
 console.log(li);
