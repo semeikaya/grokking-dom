59f7a3626649fe2f2fae361f43952f8b6a4becec
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-19 12:48:51 +0300

    update

--- create-nodes-1.5.js
+++ create-nodes-1.5.js
@@ -1,12 +1,16 @@
 const div1 = document.createElement('div')
 const div2 = document.createElement('div')
 const div3 = document.createElement('div')
+
 div1.classList.add('red')
 div2.classList.add('blue')
 div3.classList.add('green')
-div1.append(div3)
-div3.append(div2)
+
 div2.textContent = 'я вложен'
+
+div3.append(div2)
+div1.append(div3)
+
 console.log(div1);
 console.log(div2);
 console.log(div3);
